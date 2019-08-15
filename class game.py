7a8ca76b3eaab78c285e7a1b677342c4065508fc
Abdullah-Ahmed1 <54108561+Abdullah-Ahmed1@users.noreply.github.com>
2019-08-15 14:54:28 +0500

    Add files via upload

---
+++ class game.py
@@ -0,0 +1,21 @@
+print ("lets play a game of our class students")
+w=input("ask a question:   ")
+a=("the most sleepy student of a class")
+b=("talented student of a class")
+c=("papa ki pari in class")
+d=("brightest student of class")
+e=("guy with sexy long legs")
+f=(" guy with qoute word hanji")
+if w==a :
+   print("ibrahim and abdullah")
+if w==b :
+    print(" nouman shah ")
+if w==c :
+    print ("the komal pari(kumail) ")
+if w==d :
+    print ("alishba")
+if w==e :
+    print ("abdul basit sultan ")
+if w==f :
+    print ("basit ali")
+export shell
